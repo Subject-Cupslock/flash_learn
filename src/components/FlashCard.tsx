ac7b246fdb3a237127b5b40caa535d6b34ae776b
Subject-Cupslock <ruso23159@gmail.com>
2025-07-05 08:35:38 +0300

    Add FlashCard component

---
+++ src/components/FlashCard.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState } from "react";
+import "./FlashCard.css";
+
+export const FlashCard = ({
+  question,
+  answer,
+}: {
+  question: string;
+  answer: string;
+}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  return (
+    <div className="card-container" onClick={() => setIsFlipped(!isFlipped)}>
+      <div className={`card-inner ${isFlipped ? "is-flipped" : ""}`}>
+        <div className="card-face card-front drop-shadow-2xl bg-[#f1f0f0] absolute">
+          Вопрос: {question}
+        </div>
+
+        <div className="card-face card-back drop-shadow-2xl  bg-[#f1f0f0] absolute">
+          Ответ: {answer}
+        </div>
+      </div>
+    </div>
+  );
+};
